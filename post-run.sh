e8e47de83ad73def0aba49ce2c6ba2b07f43521e
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2024-09-17 18:32:01 +0800

    initial commit

---
+++ post-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+branch="autosync-$(date +%Y%m%d.%H%M%S)"
+git checkout --orphan $branch
+git add --all
+git commit -m "initial commit"
+git push origin $branch
+git branch --list | grep -v $branch | xargs git branch -D
